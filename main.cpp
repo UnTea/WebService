ef9cbbfc024e82e3e6309038cfa4c333897cdd3b
UnTea <weqqu2014@yandex.ru>
2021-09-25 21:28:53 +0300

    formalized Socket class

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 
 class Address {
 public:
+    Address() = default;
+
     explicit Address(uint16_t port_number) {
         addrinfo *res;
         addrinfo hints {
@@ -20,7 +22,7 @@ public:
             .ai_socktype = SOCK_STREAM,
         };
 
-        int result = getaddrinfo(NULL, std::to_string(port_number).c_str(), &hints, &res);
+        int result = getaddrinfo(nullptr, std::to_string(port_number).c_str(), &hints, &res);
         if (result != 0) {
             std::cerr << gai_strerror(result) << "\n";
             throw std::runtime_error("Cannot detect addresses!");
@@ -30,49 +32,57 @@ public:
             throw std::runtime_error("Cannot initialise addinfo structure!");
         }
 
-        for (auto p = res; p != NULL; p = p->ai_next) {
-            family = p->ai_family;
+        for (auto p = res; p != nullptr; p = p->ai_next) {
+            m_family = p->ai_family;
 
             if (p->ai_family == AF_INET) {
-                ipv4 = reinterpret_cast<sockaddr_in*>(p->ai_addr);
+                m_ipv4 = reinterpret_cast<sockaddr_in*>(p->ai_addr);
             } else {
-                ipv6 = reinterpret_cast<sockaddr_in6*>(p->ai_addr);
+                m_ipv6 = reinterpret_cast<sockaddr_in6*>(p->ai_addr);
             }
         }
 
     }
 
     [[nodiscard]] int GetFamily() const {
-        return family;
+        return m_family;
     }
 
     [[nodiscard]] sockaddr_in* GetIPv4() {
-        return ipv4;
+        return m_ipv4;
+    }
+
+    [[nodiscard]] const sockaddr_in* GetIPv4() const {
+        return m_ipv4;
     }
 
     [[nodiscard]] sockaddr_in6* GetIPv6() {
-        return ipv6;
+        return m_ipv6;
+    }
+
+    [[nodiscard]] const sockaddr_in6* GetIPv6() const {
+        return m_ipv6;
     }
 
     friend std::ostream& operator<< (std::ostream& stream, const Address& address);
 
 private:
-    sockaddr_in*  ipv4;
-    sockaddr_in6* ipv6;
-    int           family;
+    sockaddr_in*  m_ipv4;
+    sockaddr_in6* m_ipv6;
+    int           m_family;
 };
 
 std::ostream& operator<< (std::ostream& stream, const Address& address) {
     char ip_string[INET6_ADDRSTRLEN];
     void *ptr;
 
-    if (address.family == AF_INET) {
-        ptr = &(address.ipv4->sin_addr);
+    if (address.m_family == AF_INET) {
+        ptr = &(address.m_ipv4->sin_addr);
     } else {
-        ptr = &(address.ipv6->sin6_addr);
+        ptr = &(address.m_ipv6->sin6_addr);
     }
 
-    inet_ntop(address.family, ptr, ip_string, sizeof(ip_string));
+    inet_ntop(address.m_family, ptr, ip_string, sizeof(ip_string));
     stream << ip_string;
 
     return stream;
@@ -80,67 +90,85 @@ std::ostream& operator<< (std::ostream& stream, const Address& address) {
 
 class Socket {
 public:
-    explicit Socket(Address address) {
-        SOCKET socket_file_descriptor = socket(address.GetFamily(), SOCK_STREAM, IPPROTO_TCP);
+    Socket() = default;
 
-        if (socket_file_descriptor == -1) {
-            throw std::runtime_error("Error while creating socket!");
+    explicit Socket(int family) {
+        m_socket = ::socket(family, SOCK_STREAM, IPPROTO_TCP);
+
+        if (m_socket == -1) {
+            throw std::runtime_error("Error while creating m_socket!");
         }
+    }
 
+    void Bind(const Address& address) const {
         int bind_result;
 
         if (address.GetFamily() == AF_INET) {
-            bind_result = bind(socket_file_descriptor, reinterpret_cast<sockaddr*>(address.GetIPv4()), sizeof(sockaddr_in));
+            bind_result = bind(m_socket, reinterpret_cast<const sockaddr*>(address.GetIPv4()), sizeof(sockaddr_in));
         } else {
-            bind_result = bind(socket_file_descriptor, reinterpret_cast<sockaddr*>(address.GetIPv6()), sizeof(sockaddr_in6));
+            bind_result = bind(m_socket, reinterpret_cast<const sockaddr*>(address.GetIPv6()), sizeof(sockaddr_in6));
         }
 
         if (bind_result == -1) {
-            closesocket(socket_file_descriptor);
-            throw std::runtime_error("Error while binding socket!");
+            throw std::runtime_error("Error while binding m_socket!");
         }
+    }
 
-        int listen_result = listen(socket_file_descriptor, 8);
+    void Listen(int backlog) const {
+        int listen_result = listen(m_socket, backlog);
 
         if (listen_result == -1) {
-            closesocket(socket_file_descriptor);
-            throw std::runtime_error("Error while Listening on socket!");
+            throw std::runtime_error("Error while Listening on m_socket!");
+        }
+    }
+
+    //TODO Нужно в конструкторе по умолчанию у Address принимать family, желательно делать это из Accept
+    Socket Accept(Address& new_client_address) const {
+        SOCKET new_socket;
+        socklen_t client_address_size;
+
+        if (new_client_address.GetFamily() == AF_INET) {
+            client_address_size = sizeof(sockaddr_in);
+        } else {
+            client_address_size = sizeof(sockaddr_in6);
         }
 
-        const std::string response = "Hello World";
-
-        while (true) {
-            SOCKET new_file_descriptor;
-            socklen_t client_address_size;
-            Address new_client(81);
-
-            if (address.GetFamily() == AF_INET) {
-                client_address_size = sizeof(sockaddr_in);
-            } else {
-                client_address_size = sizeof(sockaddr_in6);
-            }
-
-            if (address.GetFamily() == AF_INET) {
-                new_file_descriptor = accept(socket_file_descriptor, reinterpret_cast<sockaddr*>(new_client.GetIPv4()), &client_address_size);
-            } else {
-                new_file_descriptor = accept(socket_file_descriptor, reinterpret_cast<sockaddr*>(new_client.GetIPv6()), &client_address_size);;
-            }
-
-            if (new_file_descriptor == -1) {
-                throw std::runtime_error("Error while Accepting on socket!");
-            }
-
-            std::cout << new_client;
-
-            int bytes_sent = send(new_file_descriptor, response.data(), response.length(), 0);
-            std::cout << " count of sent bytes: " <<  bytes_sent << std::endl;
-            closesocket(new_file_descriptor);
+        if (new_client_address.GetFamily() == AF_INET) {
+            new_socket = accept(m_socket, reinterpret_cast<sockaddr*>(new_client_address.GetIPv4()), &client_address_size);
+        } else {
+            new_socket = accept(m_socket, reinterpret_cast<sockaddr*>(new_client_address.GetIPv6()), &client_address_size);;
         }
-        closesocket(socket_file_descriptor);
+
+        if (new_socket == -1) {
+            throw std::runtime_error("Error while Accepting on socket!");
+        }
+
+        Socket socket{};
+        socket.m_socket = new_socket;
+
+        return std::move(socket);
+    }
+
+    void Send(std::string data) const {
+        if (send(m_socket, data.data(), (int) data.length(), 0) != data.size()) {
+            throw std::runtime_error("Error while Sending on socket!");
+        }
+    }
+
+    Socket(const Socket&) = delete;
+
+    Socket& operator= (const Socket&) = delete;
+
+    Socket(Socket&& new_socket)  noexcept {
+        std::swap(m_socket, new_socket.m_socket);
+    }
+
+    ~Socket() {
+        closesocket(m_socket);
     }
 
 private:
-
+    SOCKET  m_socket{};
 };
 
 
@@ -155,12 +183,20 @@ int main() {
     try {
         Address address(80);
         std::cout << address << std::endl;
-        Socket socket(address);
+        Socket socket(address.GetFamily());
+        socket.Bind(address);
+        socket.Listen(8);
+
+        while (true) {
+            Address new_client_address{};
+            Socket new_client = socket.Accept(new_client_address);
+            new_client.Send("Hello world!");
+            std::cout << new_client_address << std::endl;
+        }
+
     } catch(std::exception& e) {
         std::cout << e.what() << std::endl;
     }
 
     WSACleanup();
-
-    return 0;
 }
